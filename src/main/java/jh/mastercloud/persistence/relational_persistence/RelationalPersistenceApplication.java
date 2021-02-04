a1850465c43e96ecd9688e1cdd46c7ccbd2910d3
jaime.hernandez <jaime.hernandez@bi-geek.com>
2021-02-04 22:50:44 +0100

    Initial commit

---
+++ src/main/java/jh/mastercloud/persistence/relational_persistence/RelationalPersistenceApplication.java
@@ -0,0 +1,13 @@
+package jh.mastercloud.persistence.relational_persistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationalPersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationalPersistenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jh/mastercloud/persistence/relational_persistence/RelationalPersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package jh.mastercloud.persistence.relational_persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationalPersistenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
